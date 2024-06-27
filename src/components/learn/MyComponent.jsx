1d9c484d6eb0a54ac4d0713053fc768597ee08a3
HUY NGUYEN <huynguyen300495@gmail.com>
2024-06-27 15:22:33 -0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,18 @@
 import './style.css'
 //fragment
 const  MyComponent = () => {
+
+    //const name = " Huy ne";
+
+    const name = [1, 2, 3]
+    // const name = {
+    //     name: "Huy Nguyen",
+    //     age: 25
+    // }
     return(
     <>
-      <div> Huy Nguyen update </div>
+      <div> {JSON.stringify(name)} Huy Nguyen update </div>
+      <div>{console.log("Huy")}</div>
       <div className="child"> child</div>
     </>
     );
