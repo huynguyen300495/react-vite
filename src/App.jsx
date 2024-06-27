196c2ddd55cd1c9542cb67979d7e1048ae518a65
HUY NGUYEN <huynguyen300495@gmail.com>
2024-06-27 16:44:57 -0700

    video 35

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,24 @@ const App = () => {
     address: "hanoi",
     country: "vietnam"
   }
+
+  const addNewTodo = (name) => {
+    alert(`call me ${name}`)
+  }
+
   return (
     <div className="todo-container">
 
       <div className="todo-title">Todo List</div>
       
-      <TodoNew />
+      <TodoNew 
+      addNewTodo = {addNewTodo}
+      />
       <TodoData 
           name={name}
           age = {age}
           data = {data}
+          
       />
      
       <div className='todo-image'>
