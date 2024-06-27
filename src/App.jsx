4f9800772a3b73414ff479302c62d912ede8b62e
HUY NGUYEN <huynguyen300495@gmail.com>
2024-06-27 14:47:35 -0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const  MyComponent = () => {
+  return(
+    <div> Huy Nguyen update </div>
+  );
+}
+
+
 const App = () => {
   const [count, setCount] = useState(0)
 
-  const test = () => {
-    console.log("Huy Dep Trai")
-  }
-
-  test()
   return (
     <>
       <div>
@@ -22,6 +24,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello huy</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
