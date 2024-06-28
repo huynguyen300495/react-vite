21d36526322a1d787c6e0cc128e65537526b7e74
HUY NGUYEN <huynguyen300495@gmail.com>
2024-06-28 14:36:37 -0700

    video 48

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,15 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children:[
+      {
+        path: "/users",
+        element: <UsersPage />,
+      }, {
+        path: "/products",
+        element: <ProductsPage />,
+      }
+    ]
   },
   {
     path: "/login",
@@ -23,14 +32,7 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <RegisterPage />,
-  },
-  {
-    path: "/users",
-    element: <UsersPage />,
-  }, {
-    path: "/products",
-    element: <ProductsPage />,
-  },
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
