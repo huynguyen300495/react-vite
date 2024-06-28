b5f561bacf67cf7bdc2e1517b43aa71964141704
HUY NGUYEN <huynguyen300495@gmail.com>
2024-06-28 15:09:49 -0700

    video 52

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,18 @@ import {
 } from "react-router-dom";
 import LoginPage from './pages/login.jsx';
 import UsersPage from './pages/users.jsx';
-import ProductsPage from './pages/products.jsx';
+import BookPage from './pages/book.jsx';
 import RegisterPage from './pages/register.jsx';
 import "./styles/global.css"
 
 import TodoApp from './components/todo/todoApp.jsx';
+import ErrorPage from './pages/error.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children:[
       {
         index: true,
@@ -27,7 +29,7 @@ const router = createBrowserRouter([
         element: <UsersPage />,
       }, {
         path: "/products",
-        element: <ProductsPage />,
+        element: <BookPage />,
       }
     ]
   },
